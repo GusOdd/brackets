bc1244f633bfb761e41eb746ee856405d14b8c7d
GusOdd <gus@dynama.by>
2021-02-23 19:51:10 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const strArr = str.split('');
+  const configArr = bracketsConfig.flat();
+  const result = [];
+  for (let i = 0; i < strArr.length; i++) {
+    if (configArr.indexOf(strArr[i]) === configArr.lastIndexOf(strArr[i])) {
+      if (configArr.indexOf(strArr[i]) % 2 === 0) {
+        result.push(strArr[i]);
+      } else {
+        const last = result.pop();
+        if (last !== configArr[configArr.indexOf(strArr[i]) - 1]) {
+          return false;
+        }
+      }
+    } else {
+      if (result.indexOf(strArr[i]) === -1) {
+        result.push(strArr[i]);
+      } else if (result.lastIndexOf(strArr[i]) === result.length - 1) {
+        result.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return !result.length;
 }
